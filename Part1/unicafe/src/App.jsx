369a1d0d766d361f4277425fb40e52aa376e3d94
Georgios Gkougkoulias <georgiosgougoulias@gmail.com>
2025-04-16 22:56:21 +0300

    Unicafe : Step 2 & Step 3

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -5,14 +5,22 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
   const handleSetGood = () => {
+    const updatedGood = good + 1;
     setGood(good + 1);
+    setAll(updatedGood + neutral + bad);
   };
   const handleSetNeutral = () => {
-    setNeutral(neutral + 1);
+    const updatedNeutral = neutral + 1;
+
+    setNeutral(updatedNeutral);
+    setAll(good + updatedNeutral + bad);
   };
   const handleSetBad = () => {
-    setBad(bad + 1);
+    const updatedBad = bad + 1;
+    setBad(updatedBad);
+    setAll(good + neutral + updatedBad);
   };
 
   return (
@@ -21,7 +29,7 @@ const App = () => {
       <Button onClick={handleSetGood} text="Good" />
       <Button onClick={handleSetNeutral} text="Neutral" />
       <Button onClick={handleSetBad} text="Bad" />
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} />
     </div>
   );
 };
@@ -34,12 +42,16 @@ const Title = ({ text }) => {
   return <h1>{text}</h1>;
 };
 
-const Statistics = ({ good, neutral, bad }) => {
+const Statistics = ({ good, neutral, bad, all }) => {
+  const average = (good * 1 + neutral * 0 + bad * -1) / all;
   return (
     <div>
       <p>good: {good}</p>
       <p>neutral :{neutral}</p>
       <p>bad :{bad}</p>
+      <p>all : {all}</p>
+      <p>average : {average}</p>
+      <p>positive: {(good / all) * 100}%</p>
     </div>
   );
 };
