77daa2f18cc9213d8e8d472704b171b6caf82e1d
Georgios Gkougkoulias <georgiosgougoulias@gmail.com>
2025-04-17 11:33:25 +0300

    Add 2.1 : Course info step 6

---
+++ Part2/course-info/src/App.jsx
@@ -0,0 +1,61 @@
+const Total = (props) => <p>Number of exercises {props.total}</p>;
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header title={course.name} />
+      <Content parts={course.parts} />
+    </>
+  );
+};
+
+const Header = ({ title }) => {
+  return <h1>{title}</h1>;
+};
+
+const Content = ({ parts }) => {
+  return (
+    <ul>
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </ul>
+  );
+};
+
+const Part = ({ part }) => {
+  return (
+    <li>
+      <p>
+        {part.name} : {part.exercises}
+      </p>
+    </li>
+  );
+};
+export default App;
