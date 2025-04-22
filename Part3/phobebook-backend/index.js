607adc482de1a5cbbacedc20221ff6e99bd01eab
Georgios Gkougkoulias <georgiosgougoulias@gmail.com>
2025-04-22 22:27:16 +0300

    Add exercise 3.8 , Phone book backend step 8 . Add more functionality with morgan

--- Part3/phobebook-backend/index.js
+++ Part3/phobebook-backend/index.js
@@ -2,7 +2,12 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 app.use(express.json());
-app.use(morgan("tiny"));
+morgan.token("data", function (req, res) {
+  return JSON.stringify(req.body);
+});
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :data")
+);
 let phonebook = [
   {
     id: "1",
@@ -64,6 +69,7 @@ app.delete("/api/persons/:id", (request, response) => {
 
 app.post("/api/persons", (request, response) => {
   let newPhone = request.body;
+
   const abort =
     phonebook.some((phone) => {
       return newPhone.number === phone.number;
