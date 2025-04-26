05a3bad39e2176283472dc4c7978306c90338edc
Georgios Gkougkoulias <georgiosgougoulias@gmail.com>
2025-04-26 12:22:18 +0300

    Add exercises 3.15,3.16,3.17,3,18 All working normally .

---
+++ Part3/phonebook-back-front/models/phone.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+url = process.env.MONGODB_URI;
+console.log("connecting");
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("Failed to connect to MongoDB", error.message);
+  });
+
+const phoneSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+phoneSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Phone", phoneSchema);
