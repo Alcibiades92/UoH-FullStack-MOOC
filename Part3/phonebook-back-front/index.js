8871c009c80dc03cb4a9afc3509bfebd14a2d343
Georgios Gkougkoulias <georgiosgougoulias@gmail.com>
2025-04-27 22:13:43 +0300

    Add 3.22 So no errors appear on the backend

--- Part3/phonebook-back-front/index.js
+++ Part3/phonebook-back-front/index.js
@@ -1,104 +1,104 @@
-require("dotenv").config();
-const express = require("express");
-const Phone = require("./models/phone.js");
-const morgan = require("morgan");
-const cors = require("cors");
-const app = express();
-app.use(express.static("dist"));
-app.use(cors());
-app.use(express.json());
-morgan.token("data", function (req, res) {
-  return JSON.stringify(req.body);
-});
+require('dotenv').config()
+const express = require('express')
+const Phone = require('./models/phone.js')
+const morgan = require('morgan')
+const cors = require('cors')
+const app = express()
+app.use(express.static('dist'))
+app.use(cors())
+app.use(express.json())
+morgan.token('data', function (req) {
+  return JSON.stringify(req.body)
+})
 app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms :data")
-);
+  morgan(':method :url :status :res[content-length] - :response-time ms :data')
+)
 
-app.get("/api/persons", (request, response) => {
+app.get('/api/persons', (request, response) => {
   Phone.find({}).then((phones) => {
-    response.json(phones);
-  });
-});
-app.get("/info", (request, response) => {
+    response.json(phones)
+  })
+})
+app.get('/info', (request, response) => {
   Phone.find({}).then((phones) => {
-    const date = request.headers["date"]
-      ? new Date(request.header["date"]).toLocaleString()
-      : new Date().toLocaleString();
-    const phrase = `Phonebook has info for ${phones.length} people, \n Date and time : ${date}`;
-    response.send(phrase);
-  });
+    const date = request.headers['date']
+      ? new Date(request.header['date']).toLocaleString()
+      : new Date().toLocaleString()
+    const phrase = `Phonebook has info for ${phones.length} people, \n Date and time : ${date}`
+    response.send(phrase)
+  })
 
   // const toBeReturned = { phrase, date };
-});
-const PORT = process.env.PORT;
+})
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`This app is running on port :${PORT}`);
-});
+  console.log(`This app is running on port :${PORT}`)
+})
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
   Phone.findById(id).then((phone) => {
     if (phone) {
-      response.json(phone);
+      response.json(phone)
     } else {
-      response.status(404).end();
+      response.status(404).end()
     }
-  });
-});
+  })
+})
 
-app.delete("/api/persons/:id", (request, response, next) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Phone.findByIdAndDelete(request.params.id)
-    .then((result) => {
-      response.status(204).end();
+    .then(() => {
+      response.status(204).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
-app.post("/api/persons", (request, response, next) => {
-  const body = request.body;
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
   if (!body.name) {
     return response.status(400).json({
-      error: "Provide a number and a name!",
-    });
+      error: 'Provide a number and a name!',
+    })
   }
   const phone = new Phone({
     number: body.number,
     name: body.name,
-  });
+  })
   phone
     .save()
     .then((savedPhone) => {
-      response.json(savedPhone);
+      response.json(savedPhone)
     })
     .catch((error) => {
-      next(error);
-    });
-});
-app.put("/api/persons/:id", (request, response, next) => {
-  const { name, number } = request.body;
+      next(error)
+    })
+})
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
 
   Phone.findById(request.params.id)
     .then((phone) => {
       if (!phone) {
-        return response.status(404).end();
+        return response.status(404).end()
       }
-      phone.name = name;
-      phone.number = number;
+      phone.name = name
+      phone.number = number
 
       return phone.save().then((updatedPhone) => {
-        response.json(updatedPhone);
-      });
+        response.json(updatedPhone)
+      })
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 const errorHandler = (error, request, response, next) => {
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "Bad request" });
-  } else if (error.name === "ValidationError") {
-    console.log("Validation error");
-    return response.status(400).json({ error: error.message });
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'Bad request' })
+  } else if (error.name === 'ValidationError') {
+    console.log('Validation error')
+    return response.status(400).json({ error: error.message })
   }
-  next(error);
-};
+  next(error)
+}
 
-app.use(errorHandler);
+app.use(errorHandler)
--- Part3/phonebook-back-front/models/phone.js
+++ Part3/phonebook-back-front/models/phone.js
@@ -1,16 +1,17 @@
-const mongoose = require("mongoose");
+require('dotenv').config()
+const mongoose = require('mongoose')
 
-mongoose.set("strictQuery", false);
-url = process.env.MONGODB_URI;
-console.log("connecting");
+mongoose.set('strictQuery', false)
+const url = process.env.MONGODB_URI
+console.log('connecting')
 mongoose
   .connect(url)
-  .then((result) => {
-    console.log("Connected to MongoDB");
+  .then(() => {
+    console.log('Connected to MongoDB')
   })
   .catch((error) => {
-    console.log("Failed to connect to MongoDB", error.message);
-  });
+    console.log('Failed to connect to MongoDB', error.message)
+  })
 
 const phoneSchema = new mongoose.Schema({
   name: {
@@ -18,10 +19,10 @@ const phoneSchema = new mongoose.Schema({
     required: true,
     validate: {
       validator: function (value) {
-        return value.length >= 3;
+        return value.length >= 3
       },
       message: (props) => {
-        return `Expected at least 3 characters got ${props.value.length}`;
+        return `Expected at least 3 characters got ${props.value.length}`
       },
     },
   },
@@ -30,21 +31,21 @@ const phoneSchema = new mongoose.Schema({
     required: true,
     validate: {
       validator: function (value) {
-        return /^(?=.{8,}$)\d{2,3}-\d+$/.test(value);
+        return /^(?=.{8,}$)\d{2,3}-\d+$/.test(value)
       },
-      message: (props) => {
-        return `Pattern provided is not compatible with the correct one.`;
+      message: () => {
+        return 'Pattern provided is not compatible with the correct one.'
       },
     },
   },
-});
+})
 
-phoneSchema.set("toJSON", {
+phoneSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Phone", phoneSchema);
+module.exports = mongoose.model('Phone', phoneSchema)
