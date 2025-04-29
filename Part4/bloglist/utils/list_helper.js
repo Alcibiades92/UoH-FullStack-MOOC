e55664a106a548c3e3cff130df70796ae353c427
Georgios Gkougkoulias <georgiosgougoulias@gmail.com>
2025-04-29 17:11:36 +0300

    Add 4.6 & 4.7  . Used lodash for achieve the results.

--- Part4/bloglist/utils/list_helper.js
+++ Part4/bloglist/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require("lodash");
+
 const dummy = (blogs) => {
   return 1;
 };
@@ -15,8 +17,32 @@ const favoriteBlog = (blogs) => {
   return fav.length === 1 ? fav[0] : fav;
 };
 
+const mostBlogs = (blogs) => {
+  const groupByArray = _.groupBy(blogs, (blog) => blog.author);
+
+  const authorBlogArray = _.map(groupByArray, (value, key) => ({
+    author: key,
+    blogs: value.length,
+  }));
+  const mostBlogsAuthor = _.maxBy(authorBlogArray, "blogs");
+  console.log(mostBlogsAuthor);
+  return mostBlogsAuthor;
+};
+
+const mostLikes = (blogs) => {
+  const groupByAuthor = _.groupBy(blogs, (blog) => blog.author);
+  const authorTotalLikes = _.map(groupByAuthor, (value, key) => {
+    return { author: key, likes: totalLikes(value) };
+  });
+  console.log(authorTotalLikes);
+  const mostLikedAuthor = _.maxBy(authorTotalLikes, "likes");
+  return mostLikedAuthor;
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
+  mostLikes,
 };
