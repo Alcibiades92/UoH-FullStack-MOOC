0d833555481d4acec86718bb2de77a8ed07056e5
Georgios Gkougkoulias <georgiosgougoulias@gmail.com>
2025-05-06 12:34:47 +0300

    Add 4.21 . Deletion can happen only from the person who added the blog. the blog reference gets deleted from the user as well

--- Part4/bloglist/controllers/blogs.js
+++ Part4/bloglist/controllers/blogs.js
@@ -2,6 +2,7 @@ const blogRouter = require("express").Router();
 const Blog = require("../models/blog.js");
 const User = require("../models/user.js");
 const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 // Retrive token from the authorization headder or return null
 
 blogRouter.get("/", async (request, response, next) => {
@@ -49,10 +50,30 @@ blogRouter.post("/", async (request, response, next) => {
 });
 
 blogRouter.delete("/:id", async (request, response, next) => {
+  const blogId = request.params.id;
+  // if it misses respond
+  if (!request.token) {
+    return response.status(401).json({
+      error: "Token missing",
+    });
+  }
+  //compare the ids
   try {
-    await Blog.findByIdAndDelete(request.params.id);
-    response.status(204).end();
+    const decodedToken = jwt.verify(request.token, process.env.SECRET);
+    const blog = await Blog.findById(blogId);
+    console.log(blog.user);
+    console.log(blog.user.toString() === decodedToken.id.toString());
+    if (blog.user.toString() === decodedToken.id.toString()) {
+      await Blog.findByIdAndDelete(blogId);
+      await User.findByIdAndUpdate(decodedToken.id, {
+        $pull: { blogs: new mongoose.Types.ObjectId(blog._id) },
+      });
+      return response.status(204).end();
+    }
   } catch (exception) {
+    if (exception.name === "JsonWebTokenError")
+      return response.status(401).json({ message: "Not Authorized" });
+
     next(exception);
   }
 });
