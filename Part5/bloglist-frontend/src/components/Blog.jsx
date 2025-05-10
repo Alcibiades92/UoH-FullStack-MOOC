f64dd2414b6c8d841da2a550a610c4c4bd3c871f
Georgios Gkougkoulias <georgiosgougoulias@gmail.com>
2025-05-10 21:15:39 +0300

    Add 5.7 . Expand functioanlity for blog .SHow more or less info for the blog with a button

--- Part5/bloglist-frontend/src/components/Blog.jsx
+++ Part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,31 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
+const Blog = ({ blog }) => {
+  const [showAll, setShowAll] = useState(false);
+  console.log(blog);
+  const toggleShowAll = () => {
+    setShowAll((showAll) => !showAll);
+  };
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+  return (
+    <div style={blogStyle}>
+      <p>Title : {blog.title}</p>
 
-export default Blog
+      {showAll && (
+        <div>
+          <p>Author: {blog.author}</p>
+          <p>Url :{blog.url}</p>
+          <p>Likes : {blog.likes}</p>
+        </div>
+      )}
+      <button onClick={toggleShowAll}>{showAll ? "hide" : "view"}</button>
+    </div>
+  );
+};
+
+export default Blog;
