50e5e9a6d1792bd81429ebc66252c4c365560144
Georgios Gkougkoulias <georgiosgougoulias@gmail.com>
2025-05-27 00:10:41 +0300

    Add 6.21 and 6.22

--- Part6/query-anecdotes/src/App.jsx
+++ Part6/query-anecdotes/src/App.jsx
@@ -1,19 +1,35 @@
 import AnecdoteForm from "./components/AnecdoteForm";
 import Notification from "./components/Notification";
-import { useQuery } from "@tanstack/react-query";
-import { getAnecdotes } from "./requests";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { createAnecdote, getAnecdotes } from "./requests";
+import { updateAnecdote } from "./requests";
 const App = () => {
-  const handleVote = (anecdote) => {
-    console.log("vote");
-  };
+  const queryClient = useQueryClient();
+  const handleVote = async (anecdote) => {
+    const anecdoteToUpdate = queryClient
+      .getQueryData(["anecdotes"])
+      .find((anec) => anecdote.id === anec.id);
+    console.log(anecdoteToUpdate);
+    const updatedAnecdote = {
+      ...anecdoteToUpdate,
+      votes: anecdoteToUpdate.votes + 1,
+    };
 
-  // const anecdotes = [
-  //   {
-  //     content: "If it hurts, do it more often",
-  //     id: "47145",
-  //     votes: 0,
-  //   },
-  // ];
+    voteAnecdoteMutation.mutate(updatedAnecdote);
+    // voteAnecdoteMutation.mutate({anecdote.id})
+    // const anecdotes = queryClient.getQueryData(["anecdotes"]);
+  };
+  const voteAnecdoteMutation = useMutation({
+    mutationFn: updateAnecdote,
+    onSuccess: (updatedAnecdote) => {
+      // const anecdotes = queryClient.getQueryData(["anecdotes"]);
+      queryClient.setQueryData(["anecdotes"], (anecdotes) => {
+        return anecdotes.map((anecdote) =>
+          anecdote.id === updatedAnecdote.id ? updatedAnecdote : anecdote
+        );
+      });
+    },
+  });
   const result = useQuery({
     queryKey: ["anecdotes"],
     queryFn: getAnecdotes,
--- Part6/query-anecdotes/src/requests.js
+++ Part6/query-anecdotes/src/requests.js
@@ -5,3 +5,16 @@ const baseUrl = "http://localhost:3001/anecdotes";
 export const getAnecdotes = () => {
   return axios.get(baseUrl).then((response) => response.data);
 };
+
+export const createAnecdote = (newAnecdote) => {
+  return axios.post(baseUrl, newAnecdote).then((response) => response.data);
+};
+
+export const updateAnecdote = (updatedAnecdote) => {
+  return axios
+    .put(`${baseUrl}/${updatedAnecdote.id}`, updatedAnecdote)
+    .then((response) => {
+      console.log(response.data, response);
+      return response.data;
+    });
+};
